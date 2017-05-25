16aa992ba2a0df4de28aa605b7104993bda4d6ea
Rimian Perkins <hello@rimian.com.au>
2017-05-25 10:59:41 +1000

    initial css

--- plugin.rb
+++ plugin.rb
@@ -4,6 +4,8 @@
 # url: https://github.com/choiceaustralia/discourse-integration
 # authors: Rimian Perkins
 
+register_asset 'stylesheets/common/join.scss'
+
 load File.expand_path('../lib/discourse_integration/engine.rb', __FILE__)
 
 Discourse::Application.routes.prepend do
